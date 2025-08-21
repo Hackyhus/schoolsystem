edf1b21a9dc8e4928ca6311c791d295423932600
Hassan Abdulhamid <hassanabdulhamid603@gmail.com>
2025-08-21 05:37:41 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,17 +3,20 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBc1zl7ui6u6v2IsUmHkKtja_QzxcTXuVM",
-  authDomain: "insightconnect-portal.firebaseapp.com",
-  projectId: "insightconnect-portal",
-  storageBucket: "insightconnect-portal.firebasestorage.app",
-  messagingSenderId: "700555489196",
-  appId: "1:700555489196:web:eaf2407490ff32ef0c5409",
+  apiKey: "AIzaSyCGaM7dKTaYAAx159WjlENGASvUoQhXh-c",
+  authDomain: "haschat-31cbd.firebaseapp.com",
+  projectId: "haschat-31cbd",
+  storageBucket: "haschat-31cbd.appspot.com",
+  messagingSenderId: "419048730722",
+  appId: "1:419048730722:web:68ec923496b3046430613b",
+  measurementId: "G-MXHEHWDHKF"
 };
 
 // Initialize Firebase
@@ -21,5 +24,6 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
+const analytics = getAnalytics(app);
 
-export { app, db, auth, storage };
+export { app, db, auth, storage, analytics };
