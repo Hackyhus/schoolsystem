9553626de3386edc187d42d75d41bb47ea1116c7
Hassan Abdulhamid <hassanabdulhamid603@gmail.com>
2025-10-20 06:20:38 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,12 +7,14 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "your-api-key",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "your-auth-domain",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "your-project-id",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "your-storage-bucket",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "your-messaging-sender-id",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "your-app-id",
+  apiKey: "AIzaSyCGaM7dKTaYAAx159WjlENGASvUoQhXh-c",
+  authDomain: "haschat-31cbd.firebaseapp.com",
+  databaseURL: "https://haschat-31cbd-default-rtdb.firebaseio.com",
+  projectId: "haschat-31cbd",
+  storageBucket: "haschat-31cbd.appspot.com",
+  messagingSenderId: "419048730722",
+  appId: "1:419048730722:web:68ec923496b3046430613b",
+  measurementId: "G-MXHEHWDHKF"
 };
 
 
