f8a8a50f9d09772b60f43230bcb4ebf7d01c7ca2
Hassan Abdulhamid <hassanabdulhamid603@gmail.com>
2025-10-21 07:53:49 +0000

    why does the entire page scrol instead of the side bar like the entire s

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -116,7 +116,7 @@ export default function DashboardLayout({
             }
           }
         `}</style>
-         <div className="flex flex-1 overflow-hidden h-full">
+         <div className="flex h-full overflow-hidden">
           <div className="print-hidden">
             <DashboardSidebar />
           </div>
